8fe4df70ae750ae9b11d2a36e9b9e68bad37d609
LopezChrisBong <chrisbong.lopez@dnsc.edu.ph>
2023-10-04 23:53:30 +0800

    delete un wanted nodes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,21 @@
 import './assets/main.css'
-import './assets/stepper.scss'
+// import './assets/stepper.scss'
 import { createApp } from 'vue'
 import App from './components/App.vue'
 import router from './router'
+import store from './store'
 import vue3GoogleOauth2 from 'vue3-google-login'
-createApp(App).use(router).use(vue3GoogleOauth2,{
-    client_id: import.meta.env.VITE_GOOGLE_CLIENTID
-}).mount('#app')
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUser, faClock } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faUser, faClock);
+
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.use(vue3GoogleOauth2,{
+    client_id: import.meta.env.VITE_GOOGLE_CLIENTID
+});
+app.mount('#app');
+
 
